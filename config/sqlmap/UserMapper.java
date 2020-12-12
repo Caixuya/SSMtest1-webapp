7b27ce82c6b4930bf5833d28319221df595e8b36
Caixuya <214089030@qq.com>
2020-12-12 10:33:27 +0800

    这个项目是查询所有记录数的，查询语句和接口都写在了UserMapper.java接口文件里，分开写的在SSMtest3-webapp里面，关于这点的详细说明在这个项目的UserMapper.java里也有注释说明

--- config/sqlmap/UserMapper.java
+++ config/sqlmap/UserMapper.java
@@ -5,6 +5,16 @@ import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper {
 	
-	@Select("select count(*) from user")//有这句话可以不用UserMapper.xml文件，但是mybatis-config.xml的mappers需要改成<mappers><mapper class="sqlmap.UserMapper"/></mappers>	
+	//有这句话可以不用UserMapper.xml文件，但是mybatis-config.xml的mappers需要改成<mappers><mapper class="sqlmap.UserMapper"/></mappers>	
+	@Select("select count(*) from user")
 	public int count();
+
+/*如果必须有UserMapper.xml文件，则不用写@Select文件，需要另外创一个UserMapper.xml文件，然后写代码如下：（具体参考SSMtest3-webapp）
+  <mapper namespace="com.thxy.dao.user.UserMapper">
+	<select id="count" parameterType="int"
+		resultType="int">
+		select count(id) from t_user
+	</select>
+</mapper>*/
+	
 }
